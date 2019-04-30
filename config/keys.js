67f525f62cafcd64009c66fffc105c75ce5e8d19
simsies <jcpsimmons@gmail.com>
2019-04-29 17:10:43 -0700

    importing local keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,12 @@
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY
   };
 } else {
-  module.exports = require("./keys_dev");
+  var { localMongoURI, localSecretOrKey } = require("./keys_dev");
+  module.exports = {
+    mongoURI: localMongoURI,
+    secretOrKey: localSecretOrKey
+  };
 }
