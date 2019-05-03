24123e3162683c3a88e0c6266ec99d4d7f907220
simsies <jcpsimmons@gmail.com>
2019-05-03 11:39:10 -0700

    bug reporter

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -7,6 +7,12 @@ export default function Footer() {
       <a href="http://jcsdesign.me" className="href">
         Josh C. Simmons
       </a>
+      <a
+        href="https://github.com/jcpsimmons/greatreads/issues"
+        className="href"
+      >
+        <p>Report Bugs</p>
+      </a>
     </footer>
   );
 }
